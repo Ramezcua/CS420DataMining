3050aa7e465050aa7137dcec0fc53d49ca894f1c
Ricco Amezcua <ramezcua7@gmail.com>
2013-03-05 16:12:13 -0600

    Have the temp table showing up in bestSplit

--- Assignment 2/ass2_2.r
+++ Assignment 2/ass2_2.r
@@ -1,10 +1,22 @@
-spambase <- read.csv("./spambase/spambase.data", header=F, colClasses=c(rep('numeric',57), 'factor'))
+
+MakeSplitPoints <- function(points){
+  # Adding the first point from points as it will be the first point in split points
+  splitpoints <- points[1]
+  while (length(points) > 1){
+    splitpoints <- c(splitpoints, (points[1]+points[2])/2)
+    points <- points[-1]
+  }
+  
+  return (splitpoints)
+}
+
+spambase <- read.csv("./spambase/spambase.data", header=F, 
+                     colClasses=c(rep('numeric',57), 'factor'))
 
 
 # Naming the columns
 names(spambase)<-c("word_freq_make",
-               
-                   "word_freq_address",
+                "word_freq_address",
                 "word_freq_all",
                 "word_freq_3d",
                 "word_freq_our",
@@ -68,80 +80,78 @@ spambase$spam <- ifelse(spambase$spam == 0, c("not_spam"), c("spam"))
                 
 # Getting the samples of 80% for traning and 20% for testing
 i <- sample(2, nrow(spambase), replace = T, prob = c(0.8, 0.2))
-trainSet <- spambase[i==1,]
-testSet <- spambase[i==2,]
+train.set <- spambase[i==1,]
+test.set <- spambase[i==2,]
 
 
-best_split <- function(df){
+BestSplit <- function(df){
   # 58 is a magic number because it refers to the last row in df, which is spam in spambase
   # Didn't have time to figure out how formula works
-  att_num <- 1
-  sorted_values <- sort(df[,att_num])
-  temp_continous_frame <- data.frame(value=numeric(0), not_spam_Above=numeric(0), not_spam_Below=numeric(0), spam_Above=numeric(0), spam_Below=numeric(0))
-	best_pick <-NULL
-  checked_values <- NULL
-  split_points <- makeSplitPoints(unique(sorted_values))
-  sorted_df <- df[order(df[,att_num]),]
-  first_above_not_spam <- 0
-  first_above_spam <- 0
-  for (i in 1:nrow(sorted_df)){
-    if (sorted_df[i,58] == 'not_spam'){ first_above_not_spam <- first_above_not_spam + 1}
-    else {first_above_spam <- first_above_spam + 1}
+  att.num <- 1
+  sorted.values <- sort(df[,att.num])
+  temp.continous.frame <- data.frame(value=numeric(0), 
+                          not.spam.above=numeric(0), 
+                          not.spam.below=numeric(0), 
+                          spam.above=numeric(0), 
+                          spam.below=numeric(0))  
+	best.pick <-NULL
+  checked.values <- NULL
+  split.points <- MakeSplitPoints(unique(sorted.values))
+  sorted.df <- df[order(df[,att.num]),] # Sort the data frame by the selected attribute
+  
+  # find the spam and not spam for the first 
+  first.above.not.spam <- 0
+  first.above.spam <- 0
+  for (i in 1:nrow(sorted.df)){
+    if (sorted.df[i,58] == 'not_spam'){ first.above.not.spam <- first.above.not.spam + 1}
+    else {first.above.spam <- first.above.spam + 1}
   }
   
-  temp_continous_frame <- c(split_points[1], first_above_not_spam, 0, first_above_spam, 0)
+  # Add them to the temp data frame that will hold all values
+  temp.continous.frame <- rbind(temp.continous.frame, data.frame(
+                          value = split.points[1], 
+                          not.spam.above = first.above.not.spam, 
+                          not.spam.below = 0, 
+                          spam.above = first.above.spam, 
+                          spam.below = 0))
   
-  for (j in 2:length(split_points)){
-    while(split_points[j] > sorted_df[1,att_num]){
-      #above_not_spam <- 0 Maybe add these........
-      #above_spam <- 0
-    }
-  }
-  
-  
-  
-  #for (i in 1:length(sorted_values))
-    #if (sorted_values[i]%in%checked_values == FALSE){
-      # Add this new value to check values
-     # checked_values <- c(checked_values, sorted_values[i])
-      # Variables to be filled
-    #  not_spam_Above <- 0
-  #    not_spam_Below <- 0
- #     spam_Above <- 0
-#      #spam_Below <- 0
-      
-      # Now have to counted the spams and not_spams above and below i
-      # Change 1 in df[j,1] later
-      #for (j in 1:length(sorted_values)){
-     #   if (df[j,58] == 'not_spam' && sorted_values[i] >= df[j,1]){
-    #      not_spam_Above <- not_spam_Above + 1
-   #     }else if (df[j,58] == 'not_spam' && sorted_values[i] < df[j,1]){
-  #        not_spam_Below <- not_spam_Below + 1
- #       }else if (df[i,58] == 'spam' && sorted_values[i] >= df[j,1]){
-#          spam_Above <- spam_Above + 1
-#        }else if (df[i,58] == 'spam' && sorted_values[i] < df[j,1]){
-          #spam_Below <- spam_Below + 1
-        #}
-      #}
+  # Now go through the other split points
+  for (j in 2:length(split.points)){
+    # Values for the last entry in temp frame (used for split point checking)
+    k <- j - 1
+    #print(j)
+    ns.above <- temp.continous.frame[k, "not.spam.above"] 
+    ns.below <- temp.continous.frame[k, "not.spam.below"] 
+    s.above <- temp.continous.frame[k, "spam.above"] 
+    s.below <- temp.continous.frame[k, "spam.below"] 
     
-    #temp_continous_frame[i,](1:100) <- c(i, not_spam_Above, not_spam_Below, spam_Above, spam_Below)
-    #}
-    #print(temp_continous_frame)
-}
-
-myBinaryGini <- function(x,y){
-  return( 1 - ((x/(x+y))^2) - ((y/(x+y))^2) )
-}
-
-makeSplitPoints <- function(points){
-  # Adding the first point from points as it will be the first point in split points
-  splitpoints <- points[1]
-  while (length(points) > 1){
-    splitpoints <- c(splitpoints, (points[1]+points[2])/2)
-    points <- points[-1]
+    # Check if the first entry 
+    while(split.points[j] > sorted.df[1,att.num]){
+      if (sorted.df[1, 58] == 'notspam') {
+        # a point must be moved from not spam above to not spam below
+        ns.below <- ns.bleow + 1
+        ns.above <- ns.above - 1
+      } else if (sorted.df[1,58] == 'spam'){
+        # a point must be moved from spam above to spam below
+        s.below <- s.below + 1
+        s.above <- s.above - 1
+      }
+      # Now the entry must be erased from sorted.df so it doesn't get counted again
+      sorted.df <- sorted.df[-1,]
+    }
+    
+    # All points under split point j have been counted and so j must be added to the 
+    # temp data frame
+    temp.continous.frame <- rbind(temp.continous.frame, data.frame(
+      value = split.points[j], 
+      not.spam.above = ns.above, 
+      not.spam.below = ns.below, 
+      spam.above = s.above, 
+      spam.below = s.below))
+    
   }
   
-  return (splitpoints)
+  print(temp.continous.frame)
 }
 
-best_split(spambase)
+BestSplit(test.set)
